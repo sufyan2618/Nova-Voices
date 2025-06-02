aca9324f8ea1876fea5561eeaa5645b156a73428
Sufyan Liaqat <sufyanliaquat58@gmail.com>
2025-06-02 23:52:19 +0500

    Backend initiallized

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.send("Hello World");
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,() =>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
